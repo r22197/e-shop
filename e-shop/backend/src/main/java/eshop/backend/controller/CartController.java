8121dcf2edbcf17bbdda40d6d897ade9ed3fcaa6
Petr <petr.chromecka.s01@osu.cz>
2024-04-10 04:02:19 +0200

    změna logiky crud metod

---
+++ e-shop/backend/src/main/java/eshop/backend/controller/CartController.java
@@ -0,0 +1,35 @@
+package eshop.backend.controller;
+
+import eshop.backend.model.CartHasProduct;
+import eshop.backend.service.CartService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/shopping-cart")
+public class CartController {
+    private final CartService cartService;
+
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CartHasProduct>> getCart() {
+        return ResponseEntity.ok(cartService.getCart());
+    }
+
+    @PostMapping("/add/{id}")
+    public ResponseEntity<String> addToShoppingCart(@PathVariable Long id) {
+        cartService.addProduct(id);
+        return ResponseEntity.ok("Item added to the shopping cart.");
+    }
+
+    @DeleteMapping("/remove/{id}")
+    public ResponseEntity<String> removeFromShoppingCart(@PathVariable Long id) {
+        cartService.removeProduct(id);
+        return ResponseEntity.ok("Item removed from the shopping cart.");
+    }
+}
