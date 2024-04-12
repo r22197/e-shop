80e86407125969de974caa522856a23d2c8d845b
Petr <petr.chromecka.s01@osu.cz>
2024-04-12 21:17:55 +0200

    optimalizace kategorie pro vytvoření ve frontendu

--- e-shop/backend/src/main/java/eshop/backend/model/Category.java
+++ e-shop/backend/src/main/java/eshop/backend/model/Category.java
@@ -1,14 +1,16 @@
 package eshop.backend.model;
 
-import eshop.backend.request.CategoryDto;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Data @NoArgsConstructor
+@Data @AllArgsConstructor
 public class Category {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
@@ -16,13 +18,26 @@ public class Category {
 
     private String name;
 
-    @OneToMany(mappedBy = "category")
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Category> childCategories;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "parent_category_id")
-    private Category parentCategory;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
 
-    @OneToMany(mappedBy = "category")
+
+    @OneToMany(mappedBy = "category", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private Set<Product> products;
+
+    public Category() {
+        this.products = new HashSet<>();
+    }
+
+    public void addProduct(Product product) {
+        if (products.isEmpty()) {
+            products = new HashSet<>();
+        }
+        products.add(product);
+        product.setCategory(this);
+    }
 }
--- e-shop/backend/src/main/java/eshop/backend/repository/CategoryRepository.java
+++ e-shop/backend/src/main/java/eshop/backend/repository/CategoryRepository.java
@@ -1,18 +1,26 @@
 package eshop.backend.repository;
 
 import eshop.backend.model.Category;
+import eshop.backend.model.Product;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-    @Query("select c from Category c where c.name=:name and c.parentCategory.name=:parentCategoryName")
+    @Query("select c from Category c where c.name=:name and c.parent.name=:parentCategoryName")
     Category findCategoryByNameAndParent(
             @Param("name") String name,
             @Param("parentCategoryName") String parent
     );
+
+    @EntityGraph(attributePaths = "products")
+    Optional<Category> findById(Long id);
 
 }
