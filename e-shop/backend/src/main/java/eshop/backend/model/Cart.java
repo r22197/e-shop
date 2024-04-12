fe580406e0a66fa5d77cdce84009765e1fefdb8d
Petr <petr.chromecka.s01@osu.cz>
2024-04-12 21:13:44 +0200

    přidání výpočtu celkové ceny košíku

--- e-shop/backend/src/main/java/eshop/backend/model/Cart.java
+++ e-shop/backend/src/main/java/eshop/backend/model/Cart.java
@@ -1,22 +1,29 @@
 package eshop.backend.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
 @Entity
-@Data @NoArgsConstructor
+@Data @NoArgsConstructor @AllArgsConstructor
 public class Cart {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
 
     @OneToOne
+    @JoinColumn(name = "user_id")
     private User user;
 
     @OneToMany(mappedBy = "cart")
     private Set<CartHasProduct> cartHasProducts;
+
+    public Integer calculateTotalPrice() {
+        return cartHasProducts.stream()
+                .mapToInt(e -> (int) e.getProduct().getPrice())
+                .sum();
+    }
 }
